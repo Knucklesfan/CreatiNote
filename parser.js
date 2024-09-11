1be3a35dce7361854a6731f0656b5fe31381f408
Cade Parker <bigfootreserchersoftexas@gmail.com>
2024-09-11 12:11:27 -0500

    parser continued

--- parser.js
+++ parser.js
@@ -1,99 +1,103 @@
-function render() {
-  //welcome to the crackhead zone
-  //this is a VERY small proof of concept of our renderer, the real deal will be more than just this ofc
-  //renders to the log, but only at the specified index in the text, and only how much was told to be rendered, and removes any user typed code as well
-  //or at least, thats what its gonna do when its done, for now it just renders the whole document
-  const text = msg.innerText;
-  log.innerHTML = "";
-  // const split = text.split("\n");
-  let textCache = "";
-  let componentCache = "";
-  let element = null;
-  // console.log(text.length + " " + split.length)
-  //these two functions inside another function is just a thing we can do cuz this is JS and who gives a damn!
-  function singleStyleByte(styleByte, index, blockname) {
-    //for the folks at home confused, here's a quick breakdown of this lil equation: styleByte>>6&1
-    //tldr; styleByte is our byte described above, and we shift it right 6 times,
-    //such that the 7th bit is the last bit in the byte,
-    //and then AND it by 1 to make sure it's equal to 1 to determine if that bit is enabled or disabled.
-    //We could just hardcode the AND values here, but in favor of ease of reading, i won't do that lol
-    //its basic chemistry
 
-    // } else if (mode == 5) {
-    //   mode = 0; // Exit italic mode
-    //   element.innerText = componentCache; // Set italic text content
-    //   textCache += element.outerHTML; // Add the <i> tag and content to the textCache
-    //   componentCache = ""; // Clear component cache for future use
-    // }
-
-    if (!((styleByte >> index) & 1)) {
-      //if the next character is a ~, and bit 4 of the styleByte is NOT set to 1
-      componentCache += "<" + blockname + ">";
-      styleByte = styleByte ^ (1 << index);
-    } else {
-      if ((styleByte >> index) & 1) {
-        componentCache += "</" + blockname + ">";
-        styleByte = styleByte ^ (1 << index);
-      }
-      if (styleByte == 0) {
-        styleByte = 0;
-        textCache += componentCache;
-        componentCache = "";
-      }
+function render(text) {
+    //welcome to the crackhead zone
+    //this is a VERY small proof of concept of our renderer, the real deal will be more than just this ofc
+    //renders to the log, but only at the specified index in the text, and only how much was told to be rendered, and removes any user typed code as well
+    //or at least, thats what its gonna do when its done, for now it just renders the whole document
+    // const split = text.split("\n");
+    let textCache = "";
+    let componentCache = "";
+    let element = null;
+    // console.log(text.length + " " + split.length)
+    //these two functions inside another function is just a thing we can do cuz this is JS and who gives a damn!
+    function singleStyleByte(styleByte,index,blockname) {
+        //for the folks at home confused, here's a quick breakdown of this lil equation: styleByte>>6&1
+            //tldr; styleByte is our byte described above, and we shift it right 6 times, 
+            //such that the 7th bit is the last bit in the byte, 
+            //and then AND it by 1 to make sure it's equal to 1 to determine if that bit is enabled or disabled.
+            //We could just hardcode the AND values here, but in favor of ease of reading, i won't do that lol
+            //its basic chemistry
+            
+            // } else if (mode == 5) {
+            //   mode = 0; // Exit italic mode
+            //   element.innerText = componentCache; // Set italic text content
+            //   textCache += element.outerHTML; // Add the <i> tag and content to the textCache
+            //   componentCache = ""; // Clear component cache for future use
+            // }
+    
+        if(!(styleByte>>index&1)) { //if the next character is a ~, and bit 4 of the styleByte is NOT set to 1
+            componentCache+="<"+blockname+">"
+            styleByte = styleByte^(1<<index);
+          }
+          else {
+            if(styleByte>>index&1) {
+                componentCache+="</"+blockname+">"
+                styleByte = styleByte^(1<<index);
+    
+            }
+            if( styleByte == 0) {
+                styleByte = 0;
+                textCache += componentCache;
+                componentCache = "";
+            }
+        }
+        return styleByte
     }
-    return styleByte;
-  }
-  function doubleStyleByte(text, a, styleByte, char, indexes, blocknames) {
-    if (text.charAt(a + 1) == char && !((styleByte >> indexes[1]) & 1)) {
-      //if the next character is a star, and bit 6 of the styleByte is NOT set to 1
-      componentCache += "<" + blocknames[1] + ">";
-      styleByte = styleByte ^ (1 << indexes[1]);
-      a++; //add, so that we avoid parsing the next *
-    } else if (!((styleByte >> indexes[0]) & 1) && text.charAt(a + 1) != char) {
-      componentCache += "<" + blocknames[0] + ">";
-      styleByte = styleByte ^ (1 << indexes[0]);
-    } else {
-      if ((styleByte >> indexes[0]) & 1 && text.charAt(a + 1) != char) {
-        componentCache += "</" + blocknames[0] + ">";
-        styleByte = styleByte ^ (1 << indexes[0]);
-      } else if (text.charAt(a + 1) == char && (styleByte >> indexes[1]) & 1) {
-        componentCache += "</" + blocknames[1] + ">";
-        styleByte = styleByte ^ (1 << indexes[1]);
-        a++; //add, so that we avoid parsing the next *
-      }
-      if (styleByte == 0) {
-        styleByte = 0;
-        textCache += componentCache;
-        componentCache = "";
-      }
+    function doubleStyleByte(text,a,styleByte,char,indexes,blocknames) {
+        if(text.charAt(a+1) == char && !(styleByte>>indexes[1]&1)) { //if the next character is a star, and bit 6 of the styleByte is NOT set to 1
+            componentCache+="<"+blocknames[1]+">"
+            styleByte = styleByte^(1<<indexes[1]);
+            a++; //add, so that we avoid parsing the next *
+          }
+          else if(!(styleByte>>indexes[0]&1) && text.charAt(a+1) != char) {
+            componentCache+="<"+blocknames[0]+">"
+              styleByte = styleByte^(1<<indexes[0]);
+          }
+          else {
+            if((styleByte>>indexes[0]&1) && text.charAt(a+1) != char) {
+              componentCache+="</"+blocknames[0]+">"
+              styleByte = styleByte^(1<<indexes[0]);
+            }
+            else if(text.charAt(a+1) == char && styleByte>>indexes[1]&1) {
+              componentCache+="</"+blocknames[1]+">"
+              styleByte = styleByte^(1<<indexes[1]);
+              a++; //add, so that we avoid parsing the next *
+            }
+            if( styleByte == 0) {
+    
+              styleByte = 0;
+              textCache += componentCache;
+              componentCache = "";
+            }
+          }
+          return [styleByte,a];
+    
     }
-    return [styleByte, a];
-  }
-  // for (let i = 0; i < split.length; i++) {
-  //this runs for every line of the innertext
-  //implement this code based on https://www.markdownguide.org/cheat-sheet/
-  //here is the basic idea that I had:
-  //textcache works as the cache for text, wherein all text that is written to be added to the document is written there
-  //component cache is used while working on making an element: say for example you're writing a <a>, then during the step where the url is being written,
-  //the <a>'s url is stored in componentcache, before being finished and actually being added to the element which is made and stored in element
-  // console.log(text.length)
-  let skip = false; //if true, we skip this char during parsing, and treat it as a normal char
-  let mode = 0; //this is the character mode setter, and depending on the setting, we parse in a few different ways:
-  // 0 - Run normally, append characters if need be, but other than that we chillin
-  // 1 - We are in text mod mode, open a span and wait for a span to end here because this is just basic text editing stuff
-  // 2 - We are in URL mode and in particular are setting the URL's name during creation
-  // 3 - We are in URL mode and in particular are setting the URL's URL during creation
-  // 4 - We are in widget mode and awaiting a second $ to generate the widget
-  // 5 - We are in Style mode, but otherwise are writing normal text
-  // 6 - unusued
-  // 7 - unusued
-  // 8 - unusued
-  // 9 - unusued
-  // 10 - We are in Image mode and in particular are setting the Image's alt-text during creation
-  // 11 - We are in Image mode and in particular are setting the Image's URL during creation
-  // 12 - unusued
-  // 13 - unusued
-  // 14 - We are in checkbox mode and awaiting closure
+    // for (let i = 0; i < split.length; i++) {
+      //this runs for every line of the innertext
+      //implement this code based on https://www.markdownguide.org/cheat-sheet/
+      //here is the basic idea that I had:
+      //textcache works as the cache for text, wherein all text that is written to be added to the document is written there
+      //component cache is used while working on making an element: say for example you're writing a <a>, then during the step where the url is being written,
+      //the <a>'s url is stored in componentcache, before being finished and actually being added to the element which is made and stored in element
+      // console.log(text.length)
+      let skip = false; //if true, we skip this char during parsing, and treat it as a normal char
+      let mode = 0; //this is the character mode setter, and depending on the setting, we parse in a few different ways:
+      // 0 - Run normally, append characters if need be, but other than that we chillin
+      // 1 - We are in text mod mode, open a span and wait for a span to end here because this is just basic text editing stuff
+      // 2 - We are in URL mode and in particular are setting the URL's name during creation
+      // 3 - We are in URL mode and in particular are setting the URL's URL during creation
+      // 4 - We are in widget mode and awaiting a second $ to generate the widget
+      // 5 - We are in Style mode, but otherwise are writing normal text
+      // 6 - unusued
+      // 7 - unusued
+      // 8 - unusued
+      // 9 - unusued
+      // 10 - We are in Image mode and in particular are setting the Image's alt-text during creation
+      // 11 - We are in Image mode and in particular are setting the Image's URL during creation
+      // 12 - unusued
+      // 13 - unusued
+      // 14 - We are in checkbox mode and awaiting closure
 
   let styleByte = 0; //style is handled effectively as a very small bitwise int, and we just handle that as is
   //00000000
@@ -308,7 +312,7 @@ function render() {
     }
   }
   // }
-  log.innerHTML = textCache;
+  return textCache;
 }
 
 /*
