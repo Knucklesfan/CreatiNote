b7444f74ddbb3ea103311bb0bdfaa213c350fc38
Cade Parker <bigfootreserchersoftexas@gmail.com>
2024-09-27 13:49:04 -0500

    implemented serverside kinda

--- main.go
+++ main.go
@@ -5,7 +5,11 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
+	"html"
+	"io"
 	"log"
 	"net/http"
 )
@@ -20,12 +24,36 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 	}
 	http.ServeFile(w, r, "./frontend/build/"+r.URL.Path)
 }
+func serveDocument(w http.ResponseWriter, r *http.Request) {
+	log.Println("Got http request of document")
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var body, exception = io.ReadAll(r.Body)
+	if exception != nil {
+		http.Error(w, "Failed to read body data sent.", http.StatusBadRequest)
+		return
+	}
+	var dat map[string]interface{}
+	if err := json.Unmarshal(body, &dat); err != nil {
+		http.Error(w, "Failed to read json data sent.", http.StatusBadRequest)
+		return
+	}
+	log.Println(dat["token"].(string))
+	token := dat["token"].(string)
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(token))
+
+}
 
 func main() {
 	flag.Parse()
 	hub := newHub()
 	go hub.run()
 	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/getsheet", serveDocument)
+
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
 	})
